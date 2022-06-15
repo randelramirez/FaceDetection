13ef517d2edda9a618e7b2d75c18c6d7bd1e06b5
Randel Ramirez <randelramirez1@gmail.com>
2022-06-15 20:56:09 +0800

    Add Shared library

---
+++ FaceDetection.Messaging.InterfacesConstants/Constants/RabbitMQMassTransitConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceDetection.Messaging.InterfacesConstants.Constants
+{
+    public static class RabbitMQMassTransitConstants
+    {
+        public const string RabbitMQUri = "rabbitmq://rabbit,q:5672";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string RegiserOrderServiceQueue = "regiseter.order.command";
+    }
+}
